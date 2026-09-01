aff00c25e5c7d97325c4e9f47bc6cc974a617c3f
庞耀庭 <1701132825@qq.com>
2026-09-02 00:34:52 +0800

    fix: 迁移 SQL 去除 UTF-8 BOM 防止 PostgreSQL 语法错误

--- apps/server/prisma/migrations/20260902090000_add_model_config_models/migration.sql
+++ apps/server/prisma/migrations/20260902090000_add_model_config_models/migration.sql
@@ -1,4 +1,4 @@
-﻿-- AlterTable
+-- AlterTable
 ALTER TABLE "model_configs" ADD COLUMN "models" TEXT[] DEFAULT ARRAY[]::TEXT[];
 UPDATE "model_configs" SET "models" = ARRAY["model"] WHERE "models" = ARRAY[]::TEXT[];
 
